3f203dc709c65d235b18cb030794fd635c409573
logankronforst <logan.m.kronforst@gmail.com>
2025-10-02 13:51:37 -0500

    towers of hanoi and subset sum

---
+++ Chapter-8/Recursive-Strategies/build/recursive_strategies[1]_tests.cmake
@@ -0,0 +1,3 @@
+add_test([=[subsetSumExists.Testing]=]  /home/logan/Programming-Abstractions-In-Cpp/Chapter-8/Recursive-Strategies/build/recursive_strategies [==[--gtest_filter=subsetSumExists.Testing]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[subsetSumExists.Testing]=]  PROPERTIES WORKING_DIRECTORY /home/logan/Programming-Abstractions-In-Cpp/Chapter-8/Recursive-Strategies/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  recursive_strategies_TESTS subsetSumExists.Testing)
---
+++ Chapter-8/Recursive-Strategies/src/main.cpp
@@ -0,0 +1,63 @@
+/*
+Author: Logan Kronforst
+Date: 09/23/25
+
+"Tactics without strategy is the noise before defeat"
+- Sun Tzu, ~5th Century BCE
+
+*/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <math.h>
+#include <set>
+using namespace std; 
+
+
+bool subsetSumExists(set<int>& s, int target){
+    if (s.empty()){
+        return target == 0;
+    }
+    auto firstIter = s.begin();
+    int element = *firstIter; 
+    
+    set<int> rest = s;
+    rest.erase(rest.begin());
+
+    return subsetSumExists(rest, target)
+        || subsetSumExists(rest, target - element); 
+}
+
+
+
+
+
+void moveSingleDisk(char start, char finish);
+
+void moveTower(int n, char start, char finish, char tmp){
+    if (n == 1){
+        moveSingleDisk(start, finish);
+    } else {
+        moveTower(n - 1, start, tmp, finish);
+        moveSingleDisk(start, finish);
+        moveTower(n - 1, tmp, finish, start);
+    }
+}
+
+void moveSingleDisk(char start, char finish){
+    cout << start << "-->" << finish << endl;
+}
+
+/*--- subsetSumExists Testing Suite ----*/
+TEST(subsetSumExists, Testing){
+    set<int> s = {-2 ,1, 3, 8};
+    EXPECT_TRUE(subsetSumExists(s, 7));
+}
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    moveTower(2, 'A', 'B', 'C');
+    return RUN_ALL_TESTS();
+}

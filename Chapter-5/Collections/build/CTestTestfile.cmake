cf7f832c3defc4eff586724ef085e52c9a61b423
logankronforst <logan.m.kronforst@gmail.com>
2025-12-14 17:11:38 -0600

    collections practice

--- Chapter-5/Collections/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/logan/Desktop/CS/Programming-Abstractions-In-Cpp/Chapter-5/Collections
-# Build directory: /home/logan/Desktop/CS/Programming-Abstractions-In-Cpp/Chapter-5/Collections/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/logan/Desktop/CS/Programming-Abstractions-In-Cpp/Chapter-5/Collections/build/collections[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- Chapter-5/Collections/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/logan/Desktop/CS/Programming-Abstractions-In-Cpp/Chapter-5/Collections/build/_deps/googletest-src/googlemock
-# Build directory: /home/logan/Desktop/CS/Programming-Abstractions-In-Cpp/Chapter-5/Collections/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
